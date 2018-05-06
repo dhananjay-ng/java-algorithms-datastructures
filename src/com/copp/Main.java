4533fc0edbfe091b90575fab8174403ae4f3a2e3
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2018-05-06 13:35:31 +0530

    Added Fast I/O classes

--- src/com/copp/Main.java
+++ src/com/copp/Main.java
@@ -1,8 +1,18 @@
 package com.copp;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+       
+        InputReader ir=new InputReader(System.in);
+        int n=ir.readInt();
+        StringBuilder result=new StringBuilder();
+        result.append(n).append("\n");
+        System.out.println(result);
     }
 }

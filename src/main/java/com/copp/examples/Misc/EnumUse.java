b69999ac96b907a93d3addd21f1a470b54b96afb
Dhananjay Nagargoje <dhanu10896@gmail.com>
2018-07-16 08:49:34 +0530

    Comparator & Enum : java6 Lambda: java8

---
+++ src/main/java/com/copp/examples/Misc/EnumUse.java
@@ -0,0 +1,14 @@
+package com.copp.examples.Misc;
+
+enum Season{
+SPRING,WINTER,SUMMER,FALL
+
+}
+public class EnumUse {
+
+    public static void main(String[] args) {
+        System.out.println(Season.FALL.ordinal());
+        System.out.println(Season.FALL.name());
+    }
+
+}
---
+++ src/main/java/com/copp/examples/comparator/Apple.java
@@ -0,0 +1,9 @@
+package com.copp.examples.comparator;
+
+
+public class Apple extends Fruit {
+    public Apple(String name,int count) {
+        super(name,count);
+    }
+
+}

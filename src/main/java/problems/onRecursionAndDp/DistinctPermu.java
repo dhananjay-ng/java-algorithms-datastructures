39edcfd00bdbd6d2315bd3f41f6fe648a641b897
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-01-28 09:38:03 +0530

    on recusrion

--- src/main/java/problems/onRecursionAndDp/DistinctPermu.java
+++ src/main/java/problems/onRecursionAndDp/DistinctPermu.java
@@ -1,4 +1,50 @@
 package problems.onRecursionAndDp;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class DistinctPermu {
+    static ArrayList<ArrayList<Integer>> res;
+
+    public static ArrayList<ArrayList<Integer>> permute(ArrayList<Integer> A) {
+        res = new ArrayList<>();
+        Collections.sort(A);
+        Map<Integer, Integer> freq = new LinkedHashMap<>();
+        for (int i = 0; i < A.size(); i++) {
+            if (!freq.containsKey(A.get(i))) {
+                freq.put(A.get(i), 0);
+            }
+            freq.put(A.get(i), freq.get(A.get(i)) + 1);
+        }
+        permu(A, new ArrayList<>(), freq);
+        return res;
+    }
+
+    public static void permu(ArrayList<Integer> A, ArrayList<Integer> perm, Map<Integer, Integer> freq) {
+        if (perm.size() == A.size()) {
+            res.add(new ArrayList<>(perm));
+            return;
+        }
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            if (entry.getValue() > 0) {
+                perm.add(entry.getKey());
+                System.out.println(perm.toString());
+                freq.put(entry.getKey(), entry.getValue() - 1);
+                permu(A, perm, freq);
+                freq.put(entry.getKey(), entry.getValue() + 1);
+                perm.remove(perm.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<>();
+        a.add(1);
+        a.add(1);
+        a.add(2);
+        permute(a);
+        System.out.println(res.toString());
+    }
 }
--- src/main/java/problems/onRecursionAndDp/SubsetsWithTargetSum.java
+++ src/main/java/problems/onRecursionAndDp/SubsetsWithTargetSum.java
@@ -1,4 +1,46 @@
 package problems.onRecursionAndDp;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.ListIterator;
+
 public class SubsetsWithTargetSum {
+    public class Solution {
+        public ArrayList<ArrayList<Integer>> res;
+        int target = 0;
+
+        public ArrayList<ArrayList<Integer>> combinationSum(ArrayList<Integer> a, int b) {
+            this.target = b;
+            res = new ArrayList<>();
+            Collections.sort(a);
+            ListIterator<Integer> it = a.listIterator();
+            int prev = -1;
+            while (it.hasNext()) {
+                int next = it.next();
+                if(prev!=-1 && prev == next) {
+                    it.remove();
+                }
+                prev = next;
+            }
+            helper(a, new ArrayList<>(), 0, 0);
+            return res;
+        }
+
+
+        public void helper(ArrayList<Integer> A, ArrayList<Integer> pre,int presum, int start) {
+            if(presum == this.target) {
+                res.add(new ArrayList<>(pre));
+                return;
+            }
+            for(int i=start; i < A.size(); i++) {
+                pre.add(A.get(i));
+                presum+=A.get(i);
+                helper(A, pre, presum, ++start);
+                pre.remove(pre.size()-1);
+                presum-=A.get(i);
+            }
+        }
+    }
+
 }

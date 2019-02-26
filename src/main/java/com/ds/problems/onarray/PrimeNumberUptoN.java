9b5d50975e2c3e9c5f52cbf14e6ebce026526d2d
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-02-26 23:14:11 +0530

    prime fast

--- src/main/java/com/ds/problems/onarray/PrimeNumberUptoN.java
+++ src/main/java/com/ds/problems/onarray/PrimeNumberUptoN.java
@@ -5,7 +5,32 @@ import java.util.Collections;
 import java.util.List;
 
 public class PrimeNumberUptoN {
+    // Given n, return all primes up to and including n.
+    public static List<Integer> generatePrimesFast(int n) {
+        int size = (int) Math.floor(0.5 * ( n - 3)) + 1;
+        List<Boolean> isPrime  = new ArrayList<>(Collections.nCopies(size, true));
+        List<Integer> primes = new ArrayList<>();
 
+        if (n < 2) {
+            return Collections.emptyList();
+        }
+
+        primes.add(2);
+
+        // from now on i is value of 2i + 3
+        for (long i = 0; i < size; i++) {
+            if (isPrime.get((int)i)) {
+                int p = (((int) i * 2) + 3);
+                primes.add(p);
+                // sieving needs to start from ( 2 * i + 3)^2 which is 4*i^2 + 12i + 9 (for normal space) but for use i = 2i+3 so p2
+                //starts from 2 * i * i + 6 * i + 3
+                for (long j = ((2 * i * i) + (6 * i) +3); j < size & j > 0 ; j += p ) {
+                    isPrime.set((int) j, false);
+                }
+            }
+        }
+        return primes;
+    }
     // Uses Space O(n) otherwise good
     public static List<Integer> generatePrimes(int n) {
         List<Boolean> isPrime  = new ArrayList<>(Collections.nCopies(n + 1, true));

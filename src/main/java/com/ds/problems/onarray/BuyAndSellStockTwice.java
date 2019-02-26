3a772377d2542ecb62d6777db0898b793eadfb34
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-02-26 23:48:45 +0530

    buy and sell stock twice for max profit

---
+++ src/main/java/com/ds/problems/onarray/BuyAndSellStockTwice.java
@@ -0,0 +1,29 @@
+package com.ds.problems.onarray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuyAndSellStockTwice {
+    // space O(n) time O(n) ...
+    // make use of previous computations
+    public static double buyAndSellStockTwice(List<Double> prices) {
+        double maxTotalProfitSoFar = 0.0;
+        List<Double> firstBuySellProfit = new ArrayList<>();
+        double minSoFar = Double.MAX_VALUE;
+
+        for (int i = 0; i < prices.size(); i++) {
+            minSoFar = Math.min(minSoFar, prices.get(i));
+            maxTotalProfitSoFar = Math.max(maxTotalProfitSoFar, prices.get(i) - minSoFar);
+            firstBuySellProfit.add(maxTotalProfitSoFar);
+        }
+
+        double maxSoFar = Double.MIN_VALUE;
+        for (int i = prices.size() - 1; i > 0; i--) {
+            maxSoFar = Math.max(maxSoFar , prices.get(i));
+            maxTotalProfitSoFar = Math.max(maxTotalProfitSoFar, ((maxSoFar - prices.get(i)) + firstBuySellProfit.get( i - 1)));
+        }
+
+
+        return maxTotalProfitSoFar;
+    }
+}

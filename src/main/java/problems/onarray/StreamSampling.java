1c1b6a42fbbb058f9c21eb7bc4d1c8f07729fc25
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-04 22:57:32 +0530

    stream sampling

---
+++ src/main/java/problems/onarray/StreamSampling.java
@@ -0,0 +1,26 @@
+package problems.onarray;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+public class StreamSampling {
+    public static List<Integer> onlineRandomSample(Iterator<Integer> stream,
+                                                   int k) {
+        List<Integer> result = new ArrayList<>(k);
+        Random rk =  new Random();
+        for (int i = 0; stream.hasNext () && i < k ; ++i) {
+            result.add(stream.next());
+        }
+        int numberSoFar = k;
+        while (stream.hasNext()){
+            Integer x = stream.next();
+            int n = rk.nextInt(++numberSoFar);
+            if ( n < k ) {
+                result.set(n,x);
+            }
+        }
+        return result;
+    }
+}

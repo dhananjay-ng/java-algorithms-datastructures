dcfaf769d4fb7e042a6d61c6a52e95ba29e39fb9
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-04 23:19:59 +0530

    random sampling subset modified

--- src/main/java/problems/onarray/RandomSmaplingKElements.java
+++ src/main/java/problems/onarray/RandomSmaplingKElements.java
@@ -7,7 +7,6 @@ import java.util.Random;
 public class RandomSmaplingKElements {
     public static void randomSampling(int k, List<Integer> A) {
         Random ra = new Random();
-        if (A.size()==1 && k==1) return;
         for (int i=0;i<k;i++){
             Collections.swap(A,i,i+ra.nextInt(A.size()-i));
         }

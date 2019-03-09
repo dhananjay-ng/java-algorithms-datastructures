2da465081d715be7d99ee84c1adcb1e1f6c57d41
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-09 22:33:05 +0530

    pascal triangle code improved

--- src/main/java/problems/onarray/PascalTriangle.java
+++ src/main/java/problems/onarray/PascalTriangle.java
@@ -9,24 +9,12 @@ public class PascalTriangle {
     public static List<List<Integer>> generatePascalTriangle(int numRows) {
 
         List<List<Integer>> res = new ArrayList<>(numRows);
-        if (numRows <= 0) {
-            return res;
-        }
-        List<Integer> tempRow = new ArrayList<>();
-        tempRow.add(1);
-        res.add(tempRow);
-        for (int i = 2; i <= numRows; i++) {
-            tempRow = new ArrayList<>();
-            for (int j = 0; j < i; j++) {
-                if (j - 1 >= 0 && j <= res.get(i - 2).size() - 1) {
-                    tempRow.add(res.get(i - 2).get(j - 1) + res.get(i - 2).get(j));
-                } else {
-                    tempRow.add(1);
-                }
-
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> tempRow = new ArrayList<>();
+            for (int j = 0; j <=i; j++) {
+                tempRow.add(j - 1 >= 0 && j <i? res.get(i - 1).get(j - 1) + res.get(i - 1).get(j):1);
             }
             res.add(tempRow);
-
         }
         return res;
     }

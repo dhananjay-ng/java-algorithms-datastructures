62a75c9b06a6e00b3e7a5863aa18de45add4f3e2
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-10 11:27:34 +0530

    Base Conversion: modulus based(fast)

--- src/main/java/problems/onstring/BaseConversion.java
+++ src/main/java/problems/onstring/BaseConversion.java
@@ -2,7 +2,33 @@ package problems.onstring;
 
 public class BaseConversion {
 
+    private static int getValue(String n, int b1, boolean isNeg) {
+        int val = 0;
+        for (int i = isNeg ? 1 : 0; i < n.length(); i++) {
+            val *= b1;
+            val += Character.isDigit(n.charAt(i)) ? (n.charAt(i) - '0'):((n.charAt(i) - 'A') + 10);
+        }
+        return val;
+    }
 
+    public static char resolveMultiplier(int mul) {
+        return mul <= 9 ? (char) (mul + '0') : (char) ('A' + mul - 10);
+    }
+
+    public static String convertBase(String numAsString, int b1, int b2) {
+        boolean isNegative = numAsString.charAt(0) == '-' ? true : false;
+
+        int value = getValue(numAsString, b1, isNegative);
+        StringBuilder sb = new StringBuilder();
+        if (value==0)  return isNegative?"-0":"0";
+        while (value != 0) {
+            sb.append(resolveMultiplier(value % b2));
+            value/=b2;
+        }
+        return (isNegative?"-":"")+sb.reverse().toString();
+    }
+
+    /*
     private static int getValue(String n, int b1, boolean isNeg) {
         int val = 0;
         for (int i = isNeg ? 1 : 0; i < n.length(); i++) {
@@ -44,7 +70,7 @@ public class BaseConversion {
 
         return sb.toString();
     }
-
+*/
 
 
     public static void main(String[] args) {

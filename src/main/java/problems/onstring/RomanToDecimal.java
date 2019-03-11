1d87b34381160e612df78332bc3eb1a1bda00d28
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-11 11:09:31 +0530

    roman to decimal, using lookup

--- src/main/java/problems/onstring/RomanToDecimal.java
+++ src/main/java/problems/onstring/RomanToDecimal.java
@@ -1,7 +1,35 @@
 package problems.onstring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanToDecimal {
     public static int romanToInteger(String s) {
+        Map<Character, Integer> rules = new HashMap<Character, Integer>() {
+            {
+                put('I', 1);
+                put('V', 5);
+                put('X', 10);
+                put('L', 50);
+                put('C', 100);
+                put('D', 500);
+                put('M', 1000);
+            }
+        };
+
+        int res = 0;
+        res += rules.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (rules.get(s.charAt(i)) < rules.get(s.charAt(i + 1))) {
+                res -= rules.get(s.charAt(i));
+            } else {
+                res += rules.get(s.charAt(i));
+            }
+        }
+        return res;
+    }
+
+    public static int romanToInteger1(String s) {
         int res = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
             if (s.charAt(i) == 'I') {

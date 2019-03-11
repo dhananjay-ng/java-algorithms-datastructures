c12c430de9390623fc9185181000da15852189bf
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-11 13:14:28 +0530

    Ip address generation refactored

--- src/main/java/problems/onstring/ValidIpAddressGeneration.java
+++ src/main/java/problems/onstring/ValidIpAddressGeneration.java
@@ -1,48 +1,39 @@
 package problems.onstring;
 
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 public class ValidIpAddressGeneration {
 
     public static List<String> getValidIpAddress(String s) {
-        Set<String> res = new LinkedHashSet<>();
+        List<String> res = new ArrayList<>();
         int maxLength = s.length();
-        for (int i=2;i>=0;i--){
-            for (int j=2;j>=0;j--){
-                for (int k=2;k>=0;k--){
-                    for (int p=2;p>=0;p--){
-                        if (i+j+k+p+4 > maxLength) continue;
-                        try {
-
-
-                        int one = Integer.valueOf(s.substring(0,i+1));
-                        int two = Integer.valueOf(s.substring(i+1,i+j+2));
-                        int three = Integer.valueOf(s.substring(i+j+2,i+j+k+3));
-                        int four = Integer.valueOf(s.substring(i+j+k+3,i+j+k+p+4));
-                        if (one>255 || two>255 || three>255 || four>255) continue;
-                        else {
-                            StringBuilder sb= new StringBuilder();
-                            sb.append(one).append(".")
-                                    .append(two).append(".")
-                                    .append(three).append(".")
-                                    .append(four);
-                            if (sb.toString().length() != maxLength+3) continue;
-                            res.add(sb.toString());
-                        }
-                        } catch (Exception e){
-
+        for (int i = 1; i < 4 && i < s.length(); i++) {
+            String first = s.substring(0, i);
+            if (isValidPart(first)) {
+                for (int j = 1; i + j < s.length() && j < 4; j++) {
+                    String second = s.substring(i, i + j);
+                    if (isValidPart(second)) {
+                        for (int k = 1; i + j + k < s.length() && k < 4; k++) {
+                            String third = s.substring(i + j, i + j + k);
+                            String fourth = s.substring(i + j + k);
+                            if (isValidPart(third) && isValidPart(fourth)) {
+                                res.add(first + "." + second + "." + third + "." + fourth);
+                            }
                         }
                     }
                 }
             }
         }
 
-        List<String> ress=new ArrayList<>();
-        res.forEach(s1 -> ress.add(s1));
-        return ress;
+        return res;
+    }
+
+    public static boolean isValidPart(String part) {
+        if (part.length() > 3) return false;
+        if (part.startsWith("0") && part.length() > 1) return false;
+        int x = Integer.parseInt(part);
+        return x >= 0 && x <= 255;
     }
 
     public static void main(String[] args) {

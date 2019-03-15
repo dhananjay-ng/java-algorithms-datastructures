bf9563940c424474835e4b93fc955e2fe8b657e5
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-15 15:10:32 +0530

    Stack, Queue using Deque and List reverse print using stack

---
+++ src/main/java/javarecipies/collections/DequeUse.java
@@ -0,0 +1,33 @@
+package javarecipies.collections;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class DequeUse {
+    public static void main(String[] args) {
+        Deque<Integer> deque = new LinkedList<>();
+
+        //As Stack
+        deque.addFirst(10);
+        deque.addFirst(9);
+        deque.addFirst(8);
+        deque.addFirst(7);
+        System.out.println(deque.peek());
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeFirst());
+
+        //As Queue
+        deque.addLast(10);
+        deque.addLast(9);
+        deque.addLast(8);
+        deque.addLast(7);
+        System.out.println(deque.peek());
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeFirst());
+
+    }
+}
--- src/main/java/problems/onlinkedlist/PrintListInReverseOrder.java
+++ src/main/java/problems/onlinkedlist/PrintListInReverseOrder.java
@@ -1,6 +1,10 @@
 package problems.onlinkedlist;
 
 import datastructures.linkedlist.ListNode;
+import edu.princeton.cs.algs4.In;
+
+import java.util.Deque;
+import java.util.LinkedList;
 
 public class PrintListInReverseOrder {
     static StringBuilder res = new StringBuilder();
@@ -13,13 +17,30 @@ public class PrintListInReverseOrder {
         reversePrint(node.next);
         res.append(node.data).append("->");
     }
+
+
+    //recursive solution
+    public static void reversePrintUsingStack(ListNode<Integer> node) {
+        Deque<Integer> nodes = new LinkedList<>();
+        while (node != null){
+            nodes.addLast(node.data);
+            node = node.next;
+        }
+        while (!nodes.isEmpty()){
+            System.out.print(nodes.removeFirst());
+            if (!nodes.isEmpty()) System.out.print("->");
+        }
+    }
+
+
     public static void main(String[] args) {
         ListNode<Integer> L;
         L = new ListNode<>(11, new ListNode<>(3,
                 new ListNode<>(5, new ListNode<>(3, new ListNode<>(12, null)))));
 
-        reversePrint(L);
-        System.out.println(res.delete(res.length()-2,res.length()));
+        //reversePrint(L);
+       // System.out.println(res.delete(res.length()-2,res.length()));
+        reversePrintUsingStack(L);
     }
 }
 

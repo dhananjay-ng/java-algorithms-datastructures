9e15457e19ee3bbf9cf0dc8e6c347ebbe7b8c085
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-17 22:23:22 +0530

    ds# Unordered Max Priority Queue.

---
+++ src/main/java/datastructures/priorityqueue/UnorderedMaxPQ.java
@@ -0,0 +1,55 @@
+package datastructures.priorityqueue;
+
+
+import edu.princeton.cs.algs4.In;
+
+public class UnorderedMaxPQ<Key extends Comparable<Key>> {
+    private Key[] pq;
+    private int N;      //number of elements in priority queue
+
+    public UnorderedMaxPQ(int capacity) {
+        pq = (Key[]) new Comparable[capacity];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void insert(Key x) {
+        pq[N++] = x;
+    }
+
+    public Key delMax() {
+        int max = 0;
+        for (int i = 1; i < N; i++) {
+            if (less(max, i)) max = i;
+        }
+        exch(max, N - 1);
+        return pq[--N];
+    }
+
+    private void exch(int max, int i) {
+        Key swap = pq[max];
+        pq[max] = pq[i];
+        pq[i] = swap;
+    }
+
+
+    boolean less(int a, int b) {
+        return ((Comparable<Key>) pq[a]).compareTo(pq[b]) < 0;
+    }
+
+    public static void main(String[] args) {
+        UnorderedMaxPQ<Integer> umq = new UnorderedMaxPQ<>(4);
+        umq.insert(4);
+        umq.insert(2);
+        umq.insert(9);
+        umq.insert(8);
+
+        System.out.println(umq.delMax());
+        System.out.println(umq.delMax());
+        System.out.println(umq.delMax());
+        System.out.println(umq.delMax());
+
+    }
+}

aad5f3cfef310b57dfef37492753551dc1d75f2d
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-21 20:27:27 +0530

    #stack #leetcode496 next greater element

--- src/main/java/problems/onstack/BaseBallGame.java
+++ src/main/java/problems/onstack/BaseBallGame.java
@@ -1,10 +1,36 @@
 package problems.onstack;
-
 import java.util.Deque;
 import java.util.LinkedList;
 
 public class BaseBallGame {
     public static int calPoints(String[] ops) {
+        int currentSpace = -1;
+        Deque<Integer> bb = new LinkedList<>();
+        for (int i = 0; i < ops.length; i++) {
+            if (ops[i].equals("+")) {
+                int one = Integer.parseInt(ops[i-1]);
+                int two = Integer.parseInt(ops[i-2]);
+                ops[i] = Integer.toString(one + two);
+            } else if (ops[i].equals("D")) {
+                ops[i] = Integer.toString(Integer.parseInt(ops[i-1])*2);
+            } else if (ops[i].equals("C")) {
+                ops[i] = "";
+                currentSpace = i;
+            } else {
+                if (currentSpace!=-1){
+                    ops[currentSpace] = ops[i];
+                }
+                bb.addFirst(Integer.parseInt(ops[i]));
+            }
+        }
+        int total = 0;
+        while (!bb.isEmpty()) {
+            total += bb.removeFirst();
+        }
+        return total;
+    }
+
+    public static int calPoints1(String[] ops) {
         Deque<Integer> bb = new LinkedList<>();
         for (int i = 0; i < ops.length; i++) {
             if (ops[i].equals("+")) {
---
+++ src/main/java/problems/onstack/NextGreaterElement.java
@@ -0,0 +1,41 @@
+package problems.onstack;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class NextGreaterElement {
+    public static int[] nextGreaterElement(int[] n1, int[] n2) {
+        Map<Integer,Integer> look = new HashMap<>();
+        Deque<Integer> stack = new LinkedList<>();
+        for(int i=0;i<n2.length;i++){
+            while(stack.isEmpty() == false && stack.peek()<n2[i]){
+                look.put(stack.removeFirst(), n2[i]);
+            }
+            stack.addFirst(n2[i]);
+        }
+        while (!stack.isEmpty()) {
+            look.put(stack.removeFirst(), -1);
+        }
+        for (int i=0;i<n1.length;i++) {
+            n1[i] = look.get(n1[i]);
+        }
+        return n1;
+    }
+    //problem@https://leetcode.com/problems/next-greater-element-i
+    //O(n*m) time complexity , O(1) space.
+    public static int[] nextGreaterElement1(int[] n1, int[] n2) {
+        for(int i=0;i<n1.length;i++){
+            int j=0;
+            while(n2[j++]!=n1[i]);
+            while(j < n2.length && n2[j++] <= n1[i]);
+            if(j == n2.length)n1[i] = -1;else n1[i] = n2[j-1];
+        }
+        return n1;
+    }
+
+    public static void main(String[] args) {
+        int[] n1 = {4, 1, 2};
+        int[] n2 = {1, 3, 4, 2};
+        System.out.println(Arrays.toString(nextGreaterElement(n1,n2)));
+    }
+}

502e5618d65d67718fa483b5d0c7833ab26b6831
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-26 22:02:25 +0530

    #ds #BST to be continued

---
+++ src/main/java/datastructures/symboltables/bst/BST.java
@@ -0,0 +1,55 @@
+package datastructures.symboltables.bst;
+
+public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;
+
+    private class Node {
+        private Key key;           // sorted by key
+        private Value val;         // associated data
+        private Node left, right;  // left and right subtrees
+        private int size;          // number of nodes in subtree
+
+        public Node(Key key, Value val, int size) {
+            this.key = key;
+            this.val = val;
+            this.size = size;
+        }
+    }
+
+    public BST() {
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        return x == null ? 0 : x.size;
+    }
+    public boolean contains(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        if (key == null) throw new IllegalArgumentException("calls get() with a null key");
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if(cmp > 0) {
+            return get(x.right,key);
+        } else {
+            return x.val;
+        }
+    }
+
+}

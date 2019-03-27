dcbf0eab702f09f257c92a515c2dc46f5279935e
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-27 23:55:09 +0530

    #ds #bst

--- src/main/java/datastructures/symboltables/bst/BST.java
+++ src/main/java/datastructures/symboltables/bst/BST.java
@@ -1,5 +1,9 @@
 package datastructures.symboltables.bst;
 
+import datastructures.queue.Queue;
+
+import java.util.NoSuchElementException;
+
 public class BST<Key extends Comparable<Key>, Value> {
     private Node root;
 
@@ -30,6 +34,7 @@ public class BST<Key extends Comparable<Key>, Value> {
     private int size(Node x) {
         return x == null ? 0 : x.size;
     }
+
     public boolean contains(Key key) {
         if (key == null) throw new IllegalArgumentException("argument to contains() is null");
         return get(key) != null;
@@ -45,11 +50,125 @@ public class BST<Key extends Comparable<Key>, Value> {
         int cmp = key.compareTo(x.key);
         if (cmp < 0) {
             return get(x.left, key);
-        } else if(cmp > 0) {
-            return get(x.right,key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
         } else {
             return x.val;
         }
     }
+
+    public void put(Key key, Value value) {
+        if (key == null) throw new IllegalArgumentException("Calls put() with null key");
+        if (value == null) {
+            //delete(key);
+            return;
+        }
+        root = put(root, key, value);
+    }
+
+    private Node put(Node x, Key key, Value value) {
+        if (x == null) return new Node(key, value, 1);
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, value);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, value);
+        } else x.val = value;
+
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) return x;
+        else return min(x.left);
+    }
+
+    public Key max() {
+        if (isEmpty()) throw new NoSuchElementException("calls max() with empty symbol table");
+        return max(root).key;
+    }
+
+    private Node max(Node x) {
+        if (x.right == null) return x;
+        else return max(x.right);
+    }
+
+    public Key floor(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to floor() is null");
+        if (isEmpty()) throw new NoSuchElementException("calls floor() with empty symbol table");
+        Node x = floor(root, key);
+        if (x == null) return null;
+        else return x.key;
+    }
+
+    private Node floor(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) return x;
+        if (cmp < 0) return floor(x.left, key);
+        Node t = floor(x.right, key);
+        if (t != null) return t;
+        else return x;
+    }
+
+    public Iterable<Key> keys() {
+        Queue<Key> q = new Queue<Key>();
+        inorder(root, q);
+        return q;
+    }
+
+    private void inorder(Node x, Queue<Key> q) {
+        if (x == null) return;
+        inorder(x.left, q);
+        q.enqueue(x.key);
+        inorder(x.right, q);
+    }
+
+    public void deleteMin() {
+        if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+    public void deleteMax() {
+        if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
+        root = deleteMax(root);
+    }
+
+    private Node deleteMax(Node x) {
+        if (x.right == null) return x.left;
+        x.right = deleteMax(x.right);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+    public void delete(Key key) {
+        if (key == null) throw new IllegalArgumentException("calls delete() with a null key");
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) return null;
+
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) x.left  = delete(x.left,  key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.right == null) return x.left;
+            if (x.left  == null) return x.right;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
 
 }

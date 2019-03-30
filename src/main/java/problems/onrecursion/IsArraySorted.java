96959f939b499fee969629aef2f12288ef1d2d73
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-03-30 19:15:25 +0530

    t :#

---
+++ src/main/java/problems/onrecursion/IsArraySorted.java
@@ -0,0 +1,13 @@
+package problems.onrecursion;
+
+public class IsArraySorted {
+    static boolean isSorted(int[] A, int n) {
+        if (n == 1) return true;
+        return A[n - 1] < A[n - 2] ? false : isSorted(A, n - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1,2,5,3};
+        System.out.println(isSorted(A, A.length));
+    }
+}

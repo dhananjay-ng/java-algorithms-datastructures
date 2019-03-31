3eac6aa251935eeb95d8b2db8758b684dfaea4a2
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-03-31 19:15:25 +0530

    #recusrion : tower of hanoi

---
+++ src/main/java/problems/onrecursion/HanoiTowers.java
@@ -0,0 +1,18 @@
+package problems.onrecursion;
+
+public class HanoiTowers {
+    static void game(int n, char fromTower, char middleTower, char desitnationTower) {
+        if (n == 1) {
+            System.out.println("Move disc from Tower "+fromTower+" to Tower "+desitnationTower);
+            return;
+        }
+        game(n - 1, fromTower, desitnationTower, middleTower);
+        System.out.println("Move disc from Tower "+fromTower+" to Tower "+desitnationTower);
+
+        game(n -1, middleTower, fromTower, desitnationTower);
+    }
+
+    public static void main(String[] args) {
+        game(14, 'A', 'B', 'C');
+    }
+}

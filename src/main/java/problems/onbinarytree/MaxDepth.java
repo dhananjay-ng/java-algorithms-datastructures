2f8fc7be3feb08a37bf0bbf029053113a12010ea
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-04-05 12:50:28 +0530

    tree max tree depth

---
+++ src/main/java/problems/onbinarytree/MaxDepth.java
@@ -0,0 +1,22 @@
+package problems.onbinarytree;
+
+import edu.princeton.cs.algs4.In;
+
+public class MaxDepth {
+    public static int maxDepth(BinaryTreeNode<Integer> root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth,rightDepth)+1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxDepth(BinaryTreeUtils.getSampleTree(3)));
+    }
+    
+
+}

8d02d514ed77ddf96120e19de46379b812115e97
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-06-23 22:31:58 +0530

    HE

--- src/main/java/problems/test/TestClass.java
+++ src/main/java/problems/test/TestClass.java
@@ -1,4 +1,85 @@
 package problems.test;
 
+import java.io.*;
+import java.util.*;
+
+
 public class TestClass {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter wr = new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        for (int t_i = 0; t_i < T; t_i++) {
+            int N = Integer.parseInt(br.readLine().trim());
+            String[] arr_A = br.readLine().split(" ");
+            long[] A = new long[N];
+            for (int i_A = 0; i_A < arr_A.length; i_A++) {
+                A[i_A] = Long.parseLong(arr_A[i_A]);
+            }
+            int Q = Integer.parseInt(br.readLine().trim());
+            String[] arr_L = br.readLine().split(" ");
+            long[] L = new long[Q];
+            for (int i_L = 0; i_L < arr_L.length; i_L++) {
+                L[i_L] = Long.parseLong(arr_L[i_L]);
+            }
+            String[] arr_R = br.readLine().split(" ");
+            long[] R = new long[Q];
+            for (int i_R = 0; i_R < arr_R.length; i_R++) {
+                R[i_R] = Long.parseLong(arr_R[i_R]);
+            }
+
+            long[] out_ = solve(A, R, L);
+            System.out.print(out_[0]);
+            for (int i_out_ = 1; i_out_ < out_.length; i_out_++) {
+                System.out.print(" " + out_[i_out_]);
+            }
+            System.out.println("");
+        }
+
+        wr.close();
+        br.close();
+    }
+
+    static long[] solve(long[] A, long[] R, long[] L) {
+        int alength = A.length;
+        int qlength = R.length;
+
+        long[] result = new long[qlength];
+        HashMap<String,Long> res = new HashMap<>();
+        HashMap<Integer,Long> par = new HashMap<>();
+
+        for (int queryIter = 0; queryIter < qlength; queryIter++) {
+            long sum = 0l;
+            long ri = R[queryIter];
+            long li = L[queryIter];
+            String key = Long.toString(ri) + Long.toString(li);
+
+            if(res.containsKey(key)) {
+                result[queryIter] = res.get(key);
+                continue;
+            }
+            long inbet = (ri - li)+1;
+            long parts = (inbet) / alength;
+            long remain = (inbet) % alength;
+            long arraySum = 0;
+            if (parts > 0) {
+                for (int i = 0;i< alength;i++) {
+                    arraySum = ((A[i] % 1000000007) + (arraySum % 1000000007)) % 1000000007;
+                }
+                for (int i=1;i<=parts;i++) {
+                    sum = ((arraySum) + (sum % 1000000007)) % 1000000007;
+                }
+
+            }
+
+            for (long i = li; i <li+remain; i++) {
+                int ai =(int) ((i - 1) % alength);
+                sum = ((A[ai] % 1000000007) + (sum % 1000000007)) % 1000000007;
+            }
+            res.put(key,sum);
+            result[queryIter] = sum;
+        }
+
+        return result;
+    }
 }
--- src/main/java/websites/he/TestClass.java
+++ src/main/java/websites/he/TestClass.java
@@ -1,4 +1,208 @@
 package websites.he;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class TestClass {
+    public static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public String readLine(int length) throws IOException {
+            byte[] buf = new byte[length]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        public byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void ignore(int n) throws IOException {
+            while (n-- > 0) read();
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int data, TreeNode left, TreeNode right) {
+            this.data = data;
+            this.right = right;
+            this.left = left;
+        }
+
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        Reader in = new Reader("src\\main\\java\\websites\\he\\input.txt");
+        StringBuilder result = new StringBuilder();
+        int t = in.nextInt();
+        int r = in.nextInt();
+        in.readLine();
+        TreeNode root = new TreeNode(r, null, null);
+        for (int j = 0; j < t - 1; j += 1) {
+            String path = in.readLine();
+            int value = in.nextInt();
+            in.readLine();
+            TreeNode iter = root;
+            int i = 0;
+            for (; i < path.length() - 2; i++) {
+                if (path.charAt(i) == 'L') {
+                    iter = iter.left;
+                } else {
+                    iter = iter.right;
+                }
+            }
+            if (path.charAt(i ) == 'L') {
+                iter.left = new TreeNode(value, null, null);
+            } else {
+                iter.right = new TreeNode(value, null, null);
+            }
+        }
+        TreeNode templ = root;
+        result.append(diameter(root));
+        System.out.println(result.toString());
+
+    }
+
+    public static int getHeight(TreeNode root) {
+        if (root != null) {
+            return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+        }
+        return 0;
+    }
+    public static int getMax(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+
+    private static int diameter(TreeNode root) {
+        if (root != null) {
+            // get the left and right subtree height
+            int leftH = getHeight(root.left);
+            int rightH = getHeight(root.right);
+
+            // get the left diameter and right diameter recursively.
+            int leftDiameter = diameter(root.left);
+            int rightDiameter = diameter(root.right);
+
+            // get the max leftsubtree, rightsubtree, longest path goes through
+            // root.
+            return getMax(leftH + rightH + 1, leftDiameter, rightDiameter);
+        }
+        return 0;
+
+    }
+
 }
+

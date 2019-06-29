91e1a0e6d0e3b4273409bfa0afe3b476cea06b62
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-06-29 12:32:20 +0530

    #sorting miley score problem

---
+++ src/main/java/websites/he/MileyMaxScoreSorting.java
@@ -0,0 +1,39 @@
+package websites.he;
+
+import java.io.FileInputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class MileyMaxScoreSorting {
+    public static void main(String args[])throws Exception
+    {
+       // Scanner in = new Scanner(new FileInputStream("C:\\Fieldglass\\COPP\\src\\main\\java\\websites\\he\\input.txt"));
+
+       Scanner in=new Scanner(System.in);
+        PrintWriter out=new PrintWriter(System.out);
+        int n = in.nextInt();
+        int q = in.nextInt();
+        int t[] = new int[n];
+        int s[] = new int[n];
+
+        for (int i=0;i<n;i++) {
+            t[i] = in.nextInt();
+        }
+
+        for (int i=0;i<n;i++) {
+            s[i] = in.nextInt();
+        }
+        Arrays.sort(t);
+        for (int i=0;i<q;i++){
+            int qi = in.nextInt();
+            long timsum = 0;
+            for (int j=0;j<qi;j++){
+                timsum+=t[t.length-1-j];
+            }
+            out.println(timsum);
+        }
+        out.flush();
+    }
+}

fa779f3448e44aaf872d1f40a200695828c9f547
Dhananjay Nagargoje <dhanu10896@gmail.com>
2019-10-19 10:39:00 +0530

    leetcode circle check

--- src/main/java/problems/onstring/StringProblemTests.java
+++ src/main/java/problems/onstring/StringProblemTests.java
@@ -1,13 +1,23 @@
 package problems.onstring;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class UniqueMorseRepresentationsTest {
-    @org.junit.jupiter.api.Test
+public class StringProblemTests {
+    @Test
     public void testMorseCodes() {
         UniqueMorseRepresentations uniqueMorseRepresentations = new UniqueMorseRepresentations();
         String[] words  = {"gin", "zen", "gig", "msg"};
         assertEquals(2, uniqueMorseRepresentations.uniqueMorseRepresentations(words));
+
+    }
+    @Test
+    public void judgeCircle() {
+        JudgeCircle judgeCircle = new JudgeCircle();
+        assertEquals(true, judgeCircle.judgeCircle("UD"));
+        assertEquals(false, judgeCircle.judgeCircle("UL"));
+        assertEquals(true, judgeCircle.judgeCircle("LR"));
 
     }
 }

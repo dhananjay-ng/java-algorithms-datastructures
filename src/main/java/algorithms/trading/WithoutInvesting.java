3d10b337e8cf2f2b022f05d51e3cfd59c8cb4857
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-12-14 22:54:38 +0530

    zerodha verasity lesson 1

--- src/main/java/algorithms/trading/WithoutInvesting.java
+++ src/main/java/algorithms/trading/WithoutInvesting.java
@@ -1,6 +1,7 @@
 package algorithms.trading;
 
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class WithoutInvesting {
     public static void main(String[] args) {
@@ -13,11 +14,16 @@ public class WithoutInvesting {
 
         int retirementAmount = 0;
         int investedRetirementAmount = 0;
+        NumberFormat nf = DecimalFormat.getInstance();
+        System.out.printf("%8s  %10s  %10s  %10s\n","Year","Income","Expense","Cash Retained");
         for (int i = 2; i <= 30; i++) {
 
             save*=12;
             retirementAmount += save;
             investedRetirementAmount += save;
+            System.out.printf("%8d  %10s  %10s  %10s",
+                    i,nf.format(sal*12),nf.format(sal*12-save),nf.format(retirementAmount));
+            System.out.println();
          //   System.out.println("salary : " + sal);
          //   System.out.println("save   : " + save);
             sal = sal + ((sal*inc)/100);
@@ -26,8 +32,10 @@ public class WithoutInvesting {
             //if he invests in some schemenwith 12 % return.
             investedRetirementAmount+=(investedRetirementAmount*12/100);
 
+
         }
 
+        System.out.println();
         System.out.println("Man saves "+ DecimalFormat.getInstance().format( retirementAmount) + " if he does not invest anywhere.");
         System.out.println("Man saves "+ DecimalFormat.getInstance().format(investedRetirementAmount) + " if he  invests at 12 % per annum.");
     }

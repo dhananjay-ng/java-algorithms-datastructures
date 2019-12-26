a1bb0cf10f6aba35b41ba3768edd969dd1c5dee4
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-12-26 09:38:03 +0530

    cyclic sort

--- src/main/java/problems/patterns/cyclicsort/MissingNumber.java
+++ src/main/java/problems/patterns/cyclicsort/MissingNumber.java
@@ -1,4 +1,35 @@
 package problems.patterns.cyclicsort;
 
-public class MissingNumber {
+class MissingNumber {
+
+    public static int findMissingNumber(int[] nums) {
+
+        int i = 0 ;
+        while (i < nums.length) {
+            if (nums[i] != nums.length
+                    && nums[i] != nums[nums[i]]) {
+                swap(nums, i ,  nums[i]);
+            } else {
+                i++;
+            }
+        }
+        for (int k=0;k < nums.length;k++) {
+            if (nums[k] != k) {
+                return k;
+            }
+        }
+
+        return nums.length;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MissingNumber.findMissingNumber(new int[] { 4, 0, 3, 1 }));
+        System.out.println(MissingNumber.findMissingNumber(new int[] { 8, 3, 5, 2, 4, 6, 0, 1 }));
+    }
 }
